45a8812d3514628bda0694ea97937b3d01148d02
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-25 23:00:05 +0100

    chore: a Python script that provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""log stats from collection
+"""
+from pymongo import MongoClient
+
+
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def log_stats(mongo_collection, option=None):
+    """Provides stats about Nginx logs stored in MongoDB"""
+    if option:
+        value = mongo_collection.count_documents(
+            {
+                "method": {"$regex": option}
+            }
+        )
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents({})
+    print(f"{result} logs")
+    print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    count_result = mongo_collection.count_documents({'path': '/status'})
+    print(f"{count_result} status check")
+
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
