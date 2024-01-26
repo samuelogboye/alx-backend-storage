c2fb1f31305be6ed93e957eb179a9d62c3bd12a1
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-26 01:54:25 +0100

    chore: writing string in redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" 0x02-redis_basic/exercise.py """
+
+import redis
+
+import uuid
+from typing import Union
+
+
+class Cache():
+    """Cache class with redis"""
+
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store method
+
+        Args:
+            data (Union[str, bytes, int, float]): Data to be stored
+
+        Returns:
+            str: string
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
